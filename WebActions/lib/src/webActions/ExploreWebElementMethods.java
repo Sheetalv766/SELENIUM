4978de2cdb6d2d6eb2dd5244dd789a3076040f02
Sheetal Verma <148845414+Sheetalv766@users.noreply.github.com>
2024-09-27 14:52:38 +0530

    Create ExploreWebElementMethods.java

---
+++ WebActions/lib/src/webActions/ExploreWebElementMethods.java
@@ -0,0 +1,16 @@
+package seleniumWebActions.webActions;
+
+import org.openqa.selenium.WebElement;
+
+public class ExploreWebElementMethods {
+
+	
+	public void webElementMethods() {
+		//TODO : write "WebElement elem;"
+		WebElement elem;
+
+		//TODO : type "elem."
+		// elem.
+		
+	}
+}
