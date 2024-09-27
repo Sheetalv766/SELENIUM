7d85b8e01af47af5a19aa27c12a83d25214d137e
Sheetal Verma <148845414+Sheetalv766@users.noreply.github.com>
2024-09-27 14:45:38 +0530

    Create App.java

---
+++ Waits/lib/src/App.java
@@ -0,0 +1,13 @@
+package webActions;
+
+import java.net.MalformedURLException;
+import java.text.ParseException;
+
+public class App {
+    public static void main(String[] args) throws InterruptedException, MalformedURLException, ParseException {
+        System.out.println("From App.java");
+
+        Wait wait = new Wait();
+        wait.main(null);        
+    } 
+}
